c054b7909cc424d814db8f5d02e11d4e026222c5
Mila P <lyudmila.pa@gmail.com>
2018-03-18 13:56:49 +0200

    commit enum

---
+++ src/Apple.java
@@ -0,0 +1,18 @@
+public enum Apple {
+    A_a("A a", 13), B, C, D, E, A( );
+    String name;
+    double price;
+
+    Apple(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    Apple() {
+    }
+
+
+    }
+
+
+
---
+++ src/MainEnum.java
@@ -0,0 +1,40 @@
+public class MainEnum {
+    public static void main(String[] args) {
+        //  Apple a = Apple.A;
+        Apple ap;
+        ap = Apple.A;
+        if (ap == Apple.A)
+            System.out.println("ap contains A.\n");
+        switch (ap) {
+            case A:
+                System.out.println("A is 1");
+                break;
+            case B:
+                System.out.println("B is 1");
+                break;
+            case C:
+                System.out.println("C is 1");
+                break;
+            case D:
+                System.out.println("D is 1");
+                break;
+        }
+        ap = Apple.valueOf("A");
+        Apple allapple[] = Apple.values();
+        for (Apple a : allapple) {
+            System.out.println(a);
+        }
+
+        for (Apple apple : Apple.values())
+            System.out.println(apple + " " + apple.ordinal());
+
+        ap=Apple.A;
+        Apple ap2 = Apple.B;
+        Apple ap3=Apple.C;
+        if(ap2.compareTo(ap3)<0)
+            System.out.println(ap + " comes before " + ap2);
+
+String usd = "A";
+
+    }
+}
